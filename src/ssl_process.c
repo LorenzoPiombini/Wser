8302a850c8a2d51eb0335a71ec0aba3016ee122c
Lorenzo Piombini <piombinilorenzo1987@gmail.com>
2026-01-03 10:59:50 -0500

    add(SSL): dedicated process 0.3

--- src/ssl_process.c
+++ src/ssl_process.c
@@ -34,132 +34,20 @@ int SSL_work_process(struct Connection_data *cd,int cli_sock,struct Request *req
 			for(nfds_t j = 0; j < open_fds;j++){
 				if(pfds[j].revents == 0) break;
 				switch(r){
-					case HANDSHAKE:
-					{		
-							int i;
-							for(i = 0; i < MAX_CON_DAT_ARR; i++){
-								if(cd[i].fd == cli_sock) break;
-							}
-							if(i >= MAX_CON_DAT_ARR) return 0;
-
-							if(cd[i].retry_handshake){
-								/*retry handshake*/
-								int r = 0;
-								if((r = cd[i].retry_handshake(cd[i].ssl)) <= 0){ 
-									int err = SSL_get_error(cd[i].ssl,r);
-									if(err == SSL_ERROR_WANT_READ || err == SSL_ERROR_WANT_WRITE){
-										continue;	
-									}else{
-										fprintf(stderr,"the error happens when retrying handshake\n");
-										ERR_print_errors_fp(stderr);
-										SSL_free(cd[i].ssl);
-										cd[i].fd = -1;
-										cd[i].ssl = NULL;
-										cd[i].retry_handshake = NULL;
-										cd[i].retry_read = NULL;
-										return 0;
-									}
-								}
-								cd[i].retry_handshake = NULL;
-								int result;
-								size_t bread = 0;
-								if((result = SSL_peek_ex(cd[i].ssl,req->req,BASE,&bread)) == 0) {
-									int err = SSL_get_error(cd[i].ssl,result);
-									if(err == SSL_ERROR_WANT_READ || err == SSL_ERROR_WANT_WRITE) {
-										cd[i].retry_read = SSL_peek_ex;
-										continue;
-									}else if (bread == BASE){
-										fprintf(stderr,"the issue is not enogh space in the buffer\n");
-										ERR_print_errors_fp(stderr);
-										SSL_free(cd[i].ssl);
-										cd[i].fd = -1;
-										cd[i].ssl = NULL;
-										cd[i].retry_handshake = NULL;
-										cd[i].retry_read = NULL;
-										return 0;
-									}else{
-										fprintf(stderr,"the error happens when reading SSL after handshake\n");
-										ERR_print_errors_fp(stderr);
-										SSL_free(cd[i].ssl);
-										cd[i].fd = -1;
-										cd[i].ssl = NULL;
-										cd[i].retry_handshake = NULL;
-										cd[i].retry_read = NULL;
-										return 0;
-									}
-								}
-							}	
-							break;
-						}
-					case SSL_READ_E:
-						{
-							int i;
-							for(i = 0; i < MAX_CON_DAT_ARR; i++){
-								if(cd[i].fd == cli_sock) break;
-							}
-
-							if(i >= MAX_CON_DAT_ARR) continue;
-
-							if(cd[i].retry_read){ 
-								int result;
-								size_t bread = 0;
-								if((result = cd[i].retry_read(cd[i].ssl,req->req,BASE,&bread)) == 0){
-									int err = SSL_get_error(cd[i].ssl,result);
-									if(err == SSL_ERROR_WANT_READ || err == SSL_ERROR_WANT_WRITE) {
-										continue;
-									}else{
-										fprintf(stderr,"the error happens when retrying read\n");
-										ERR_print_errors_fp(stderr);
-										SSL_free(cd[i].ssl);
-										cd[i].fd = -1;
-										cd[i].ssl = NULL;
-										cd[i].retry_handshake = NULL;
-										cd[i].retry_read = NULL;
-										return 0;
-									}
-								}
-
-								cd[i].retry_read = NULL;
-								if(bread == BASE){
-									fprintf(stderr,"buffer is not big enough\n");
-									/*TODO: read the socket again*/
-								}
-								if(handle_request(req) == BAD_REQ){
-									if(req->method == -1) return BAD_REQ;
-									if(req->size < (ssize_t)BASE) return BAD_REQ;
-
-									if(req->size == (ssize_t)BASE){
-										if(set_up_request(bread,req) == -1) return -1;
-
-										/*TODO: rewrite this case according to SSL */
-										ssize_t move = req->size;
-#if 0
-										if((bread = read(cli_sock,req->d_req +  move,req->size)) == -1){
-											if(errno == EAGAIN || errno == EWOULDBLOCK) {
-												int e = errno;
-												/*TODO ADD FD TO POLL SYS*/
-
-												continue;
-											}
-											fprintf(stderr,"(%s): cannot read data from socket",prog);
-											return 0;
-										}
-#endif
-									}
-								}
-							}
-							break;
-						}
-					case 0:
-						{
-							/*process request*/
-
-
-							break;
-						}
-					case SSL_SET_E:
-					default:
-						return 0;
+				case SSL_READ_E:
+				case HANDSHAKE:
+				{		
+					r = handle_ssl_steps(cd,cli_sock,req,ssl,ctx,pfds,j);
+					break;
+				}
+				case 0:
+				{
+					/*process request*/
+					break;
+				}
+				case SSL_SET_E:
+				default:
+					return 0;
 				}
 			}
 		}
@@ -207,7 +95,7 @@ static int handle_ssl_steps(struct Connection_data *cd,
 				 * and return;
 				 * */
 				/*ADD FD TO POLL SYS*/
-				pfd[poll_index].fd == cli_sock;
+				pfd[poll_index].fd = cli_sock;
 				pfd[poll_index].events = POLLIN;
 
 				int i;
@@ -241,9 +129,8 @@ static int handle_ssl_steps(struct Connection_data *cd,
 				 * socket is not ready add the file descriptor to the epoll system
 				 * */
 
-				/*TODO: add fd to poll*/
 
-				pfd[poll_index].fd == cli_sock;
+				pfd[poll_index].fd = cli_sock;
 				pfd[poll_index].events = POLLIN;
 				int i;
 				for(i = 0; i < MAX_CON_DAT_ARR;i++){
