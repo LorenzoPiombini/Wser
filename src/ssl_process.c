cb71907cb054aa86af151b36579737e536cbd6a8
Lorenzo Piombini <piombinilorenzo1987@gmail.com>
2026-01-03 12:16:52 -0500

    add(SSL): dedicated process 0.6

--- src/ssl_process.c
+++ src/ssl_process.c
@@ -5,41 +5,94 @@
 #include "ssl_process.h"
 #include "network.h"
 #include "request.h"
+#include "response.h"
 
 /*USE POLL for this process*/
 static int handle_ssl_steps(struct Connection_data *cd, 
 							int cli_sock,
 							struct Request *req,
 							SSL **ssl,
-							SSL_CTX **ctx,
-							struct pollfd *pfd,int poll_index);
+							SSL_CTX **ctx);
 
 int SSL_work_process(struct Connection_data *cd,int cli_sock,struct Request *req,SSL **ssl,SSL_CTX **ctx)
 {
-	nfds_t open_fds = 1;
 
-	struct pollfd pfds[20] = {0};
 	int b_out = 0;
 	for(;;){
-		int r = handle_ssl_steps(cd,cli_sock,req,ssl,ctx,pfds,0);
+		int r = handle_ssl_steps(cd,cli_sock,req,ssl,ctx);
 			
 		if(r == -1) return 0;
-		if(r == 0){
-			/*process the request*/
-			return 0;
-		}
 
 		switch(r){
 		case SSL_READ_E:
 		case HANDSHAKE:
 		{		
-			r = handle_ssl_steps(cd,cli_sock,req,ssl,ctx,pfds,0);
+			r = handle_ssl_steps(cd,cli_sock,req,ssl,ctx);
 			break;
 		}
 		case 0:
 		{
 			/*process request*/
+			switch(req->method){
+			case GET:
+			{
+				struct Response res = {0};
+				struct Content cont = {0};
+				/* Load content */	
+				if(load_resource(req->resource,&cont) == -1){
+					/*send not found response*/
+					if(generate_response(&res,404,NULL,req) == -1) break;
+
+					int w = 0;
+					if((w = write_cli_SSL(cli_sock,&res,cds)) == -1) break;
+					if( w == SSL_WRITE_E){
+						uint8_t ws = 0;
+						while((w = write_cli_SSL(cli_sock,&res,cds)) == -1){
+							if( w == SSL_WRITE_E) continue;
+						}
+
+						return 0;
+					}
+				}
+
+				/*send 200 response*/
+				if(generate_response(&res,OK,&cont,req) == -1) {
+					clear_content(&cont);
+					return 0;
+				}
+
+				clear_content(&cont);
+				int w = 0;
+				if((w = write_cli_SSL(cli_sock,&res,cds)) == -1) break;
+				if(w == SSL_WRITE_E){
+					uint8_t ws = 0;
+					while((w = write_cli_SSL(cli_sock,&res,cds)) == -1){
+						if(w == SSL_WRITE_E) continue;
+
+					}
+					clear_request(req);
+					clear_response(&res);
+					return 0;
+				}
+				return 0;
+			}
+			default:
+			}
 			break;
+		}
+		case BAD_REQ:
+		{
+			struct Response res = {0};
+			/*send a bed request response*/
+			if(generate_response(&res,400,NULL,req) == -1) break;
+			int w = 0;
+			if((w = write_cli_SSL(cli_sock,&res,cds)) == -1) break;
+			if(w == SSL_WRITE_E){
+				while((w = write_cli_SSL(cli_sock,&res,cds)) == -1){
+					if( w == SSL_WRITE_E) continue;
+				}
+			}
+			return 0;
 		}
 		case SSL_SET_E:
 		default:
@@ -58,8 +111,7 @@ static int handle_ssl_steps(struct Connection_data *cd,
 							int cli_sock,
 							struct Request *req,
 							SSL **ssl,
-							SSL_CTX **ctx,
-							struct pollfd *pfd,int poll_index)
+							SSL_CTX **ctx)
 {
 	int i;
 	for(i = 0; i < MAX_CON_DAT_ARR; i++){
@@ -84,14 +136,6 @@ static int handle_ssl_steps(struct Connection_data *cd,
 		if((hs_res = SSL_accept(*ssl)) <= 0) {
 			int err = SSL_get_error(*ssl,hs_res);
 			if(err == SSL_ERROR_WANT_READ || err == SSL_ERROR_WANT_WRITE) {
-				/* 
-				 * socket is not ready
-				 * so we add the file descriptor to the epoll system
-				 * and return;
-				 * */
-				/*ADD FD TO POLL SYS*/
-				pfd[poll_index].fd = cli_sock;
-				pfd[poll_index].events = POLLIN;
 
 				int i;
 				for(i = 0; i < MAX_CON_DAT_ARR;i++){
@@ -120,13 +164,6 @@ static int handle_ssl_steps(struct Connection_data *cd,
 		if((result = SSL_peek_ex(*ssl,req->req,BASE,&bread)) == 0) {
 			int err = SSL_get_error(*ssl,result);
 			if(err == SSL_ERROR_WANT_READ || err == SSL_ERROR_WANT_WRITE) {
-				/* 
-				 * socket is not ready add the file descriptor to the epoll system
-				 * */
-
-
-				pfd[poll_index].fd = cli_sock;
-				pfd[poll_index].events = POLLIN;
 				int i;
 				for(i = 0; i < MAX_CON_DAT_ARR;i++){
 					if(cd[i].fd == 0 || cd[i].fd == -1){
